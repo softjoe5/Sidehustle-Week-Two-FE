f4f5eaf3ddf6b0afec6b1c2180f8b5a972543238
Joe <joedeola@gmail.com>
2023-03-19 00:44:21 +0100

    Design with CSS

---
+++ index.js
@@ -0,0 +1,35 @@
+// // function message(){
+// //     var Name = document.getElementById('name');
+// //     var email = document.getElementById('email');
+// //     var phoneNumber = document.getElementById('phone number');
+// //     var male = document.getElementById('male');
+// //     var female = document.getElementById('female');
+// //     var password = document.getElementById('password');
+
+// //     const success = document.getElementById('success');
+// //     const danger = document.getElementById('danger');
+
+// //     if (Name.value === '' || email.value === '' || phoneNumber.value === '' || male.value === '' || 
+// //     female.value === '' || password.value === ''){
+// //         danger.style.display = 'block';
+// //     }
+// //     else {
+// //         setTimeout(() => {
+// //             Name.value = '';
+// //             email = '';
+// //             phoneNumber = '';
+// //             male = '';
+// //             female = '';
+// //             password = '';
+// //         }, 2000);
+
+// //         success.style.display = 'block';
+// //     }
+// // }
+
+
+// $(document).ready(function(){
+//     $('.form').on('submit', function(){
+//         alert('Your details were successfully received.');
+//     });
+// });
